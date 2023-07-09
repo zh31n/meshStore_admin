c10f136144f29a7977fd7945e1fbd6d54ff0bc58
Roman <ketzalec@gmail.com>
2023-07-10 01:00:41 +0300

    change

--- src/Pages/Users/Users.tsx
+++ src/Pages/Users/Users.tsx
@@ -1,48 +1,54 @@
-import React from 'react';
-import s from './Users.module.scss';
-import i_search from '../../assets/Search.svg';
-import ava_img from '../../assets/ava_uu.png';
+import { useState } from "react";
+import s from "./Users.module.scss";
+import i_search from "../../assets/Search.svg";
+import ava_img from "../../assets/ava_uu.png";
 import UserItemG from "../../Components/UserItemG/UserItemG";
 
-const Users = (props: any) => {
+const Users = () => {
+  const [view, setView] = useState<boolean>(false);
 
-    const data = {
-        header: [
-            {name: 'Сотрудники'},
-            {name: 'Клиенты'},
-        ],
-        users: [
-            {name: 'Фёдор Шишкин', id: 1, image: ava_img},
-            {name: 'Фёдор Шишкин', id: 1, image: ava_img},
-            {name: 'Фёдор Шишкин', id: 1, image: ava_img},
-            {name: 'Фёдор Шишкин', id: 1, image: ava_img},
-            {name: 'Фёдор Шишкин', id: 1, image: ava_img},
-        ]
+  const data = {
+    users: [
+      { name: "Фёдор Шишкин", id: 1, image: ava_img },
+      { name: "Фёдор Шишкин", id: 1, image: ava_img },
+      { name: "Фёдор Шишкин", id: 1, image: ava_img },
+      { name: "Фёдор Шишкин", id: 1, image: ava_img },
+      { name: "Фёдор Шишкин", id: 1, image: ava_img },
+    ],
+  };
 
-    }
-
-
-    const navItems = data.header.map(i => <div className={s.nav_item}>{i.name}</div>)
-
-    return (
-        <>
-            <div className={s.input_search}>
-                <img src={i_search} alt="icon search"/>
-                <input type="text" placeholder='Поиск по группам'/>
-            </div>
-            <div className={s.nav_users}>
-                {navItems}
-            </div>
-            <div className={s.cont}>
-                <div className={s.users}>
-                    <UserItemG name={'Фёдор Шишкин'} id={1} image={ava_img} />
-                </div>
-                <div className={s.left}>
-
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={s.input_search}>
+        <img src={i_search} alt="icon search" />
+        <input type="text" placeholder="Поиск по группам" />
+      </div>
+      <div className={s.nav_users}>
+        <div
+          className={view == true ? s.nav_item : s.nav_item_active}
+          onClick={() => {
+            setView(!view);
+          }}
+        >
+          Сотрудники
+        </div>
+        <div
+          className={view == true ? s.nav_item_active : s.nav_item}
+          onClick={() => {
+            setView(!view);
+          }}
+        >
+          Клиенты
+        </div>
+      </div>
+      <div className={s.cont}>
+        <div className={s.users}>
+          <UserItemG name={"Фёдор Шишкин"} id={1} image={ava_img} />
+        </div>
+        <div className={s.left}></div>
+      </div>
+    </>
+  );
 };
 
 export default Users;
