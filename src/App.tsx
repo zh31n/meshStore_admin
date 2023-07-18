84464131ad986692d4448334a240f4577b48768e
Roman <ketzalec@gmail.com>
2023-07-18 12:42:35 +0300

    chages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import './App.css'
-import {Routes, Route} from "react-router-dom";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Profile from "./Pages/Profile/Profile";
 
-const App = (props) => {
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path={"/"} element={<Login />} />
+        <Route path={"/profile/*"} element={<Profile />} />
+      </Routes>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Routes>
-                <Route path={'/'} element={<Login />}/>
-                <Route path={'/profile/*'} element={<Profile />}/>
-            </Routes>
-        </>
-    )
-}
-
-export default App
+export default App;
--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 import Header from "../../Components/Header/Header";
-import s from './Profile.module.scss'
-import {Routes, Route} from "react-router-dom";
+import s from "./Profile.module.scss";
+import { Routes, Route } from "react-router-dom";
 import Today from "../Today/Today";
 import Users from "../Users/Users";
 
 const Profile = () => {
-    return (
-        <div className={'container'}>
-            <Header/>
-            <div className={s.content}>
-                <Routes>
-                    <Route element={<Today/>} path={'/today/*'}/>
-                    <Route element={<Users/>} path={'/clients'}/>
-                </Routes>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"container"}>
+      <Header />
+      <div className={s.content}>
+        <Routes>
+          <Route element={<Today />} path={"/today/*"} />
+          <Route element={<Users />} path={"/clients"} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
